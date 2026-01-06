58d821f34be1883bb30e8db4a652c264a347d05c
sarahdenkert01-hue <sarahdenkert01@gmail.com>
2026-01-06 15:44:44 -0600

    Add month constants for easy reference

---
+++ src/constants/months.js
@@ -0,0 +1,14 @@
+export const MONTH_NAMES = [
+  'jan', 'feb', 'mar', 'apr', 'may', 'jun',
+  'jul', 'aug', 'sep', 'oct', 'nov', 'dec'
+];
+
+export const MONTHS = {
+  JAN: 0, FEB: 1, MAR: 2, APR: 3, MAY: 4, JUN: 5,
+  JUL: 6, AUG: 7, SEP: 8, OCT: 9, NOV: 10, DEC: 11
+};
+
+export const MONTH_OFFSETS = {
+  jan: 0, feb: 1, mar: 2, apr:  3, may: 4, jun: 5,
+  jul: 6, aug: 7, sep: 8, oct:  9, nov: 10, dec: 11
+};
