86dca7fafdebd1188a66adbf7f20691581fff77a
sarahdenkert01-hue <sarahdenkert01@gmail.com>
2026-01-07 16:26:49 -0600

    Add canvas, grid, and tab configuration constants

---
+++ src/constants/canvasConfig.js
@@ -0,0 +1,21 @@
+export const CANVAS_CONFIG = {
+  WIDTH: 1620,
+  HEIGHT: 2160,
+  VIEW_SCALE: 0.4,
+};
+
+export const GRID_CONFIG = {
+  cellWidth: 155,
+  cellHeight: 125,
+  startX: 254,
+  startY: 740,
+  rows: 6,
+  cols: 7,
+};
+
+export const TAB_CONFIG = {
+  height: 80,
+  width:  120,
+  startX: 0,
+  startY: 360,
+};
