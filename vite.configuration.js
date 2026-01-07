76764dade0904775ebe5ac43fc55d864a2378dee
sarahdenkert01-hue <sarahdenkert01@gmail.com>
2026-01-07 16:23:47 -0600

    Add Vite configuration for React project

---
+++ vite.configuration.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'konva-vendor':  ['react-konva', 'konva'],
+          'pdf-vendor': ['jspdf', 'pdf-lib']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-konva', 'konva', 'jspdf', 'pdf-lib']
+  }
+})
