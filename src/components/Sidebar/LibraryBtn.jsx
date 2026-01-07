56724849af451d42d4066cb1b56797c4e9b83d55
sarahdenkert01-hue <sarahdenkert01@gmail.com>
2026-01-06 20:31:45 -0600

    Add LibraryBtn component for sidebar

---
+++ src/components/Sidebar/LibraryBtn.jsx
@@ -0,0 +1,21 @@
+zimport React from 'react';
+
+export default function LibraryBtn({ onClick, children }) {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        width: "100%",
+        padding: "6px",
+        background: "#fff",
+        border: "1px solid #e2e8f0",
+        textAlign: "left",
+        cursor: "pointer",
+        fontSize: "11px",
+        marginBottom: '2px'
+      }}
+    >
+      {children}
+    </button>
+  );
+}
